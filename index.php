5d6800385062f798517e6e3684db25d9d25865be
rorschach59 <remi.pouly@free.fr>
2018-10-06 10:34:52 +0000

    [DEV] fail git, rewrite index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 
-require 'controller/DefaultController.php';
-use Controller\DefaultController;
+// Require libraries
+require 'vendor/autoload.php';
 
-$default = new DefaultController();
-var_dump($default->showIndex());
+// Require auto controller
+spl_autoload_register(function($class) {
+    $file = 'controller/'.substr($class, 11).'.php';
+    
+    if(file_exists($file)) {
+        require $file;
+    }
+});
+
+const PATH_VIEWS = 'view';
+
+// Init the router
+$engine = new \League\Plates\Engine(PATH_VIEWS);
+$sidebarController = new Controller\SidebarController();
+$sidebarContent = $sidebarController->getMenuContent();
+
+echo $engine->render('common/header', compact(''));
+echo $engine->render('common/sidebar', compact('sidebarContent'));
+echo $engine->render('common/footer', compact(''));
