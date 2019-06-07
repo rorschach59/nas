969e3e0b15a9c860a243890a4107dfd117589e68
rorschach59 <remi.pouly@free.fr>
2019-06-07 16:49:59 +0000

    [GITPOD] test

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-
+/*
 require 'controller/DefaultController.php';
 use Controller\DefaultController;
 
 $default = new DefaultController();
 var_dump($default->showIndex());
+*/
+
+echo "tata";
